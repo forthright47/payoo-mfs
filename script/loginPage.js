d765b12eb14c728ba06d58d06affb85c9118fc16
forthright47 <k.tuhinjaman@gmail.com>
2026-02-20 04:00:52 +0600

    implement login functionality in the login page

---
+++ script/loginPage.js
@@ -0,0 +1,28 @@
+// console.log("Login functionality loading");
+
+document.getElementById("login-btn").addEventListener("click", function(){
+    // console.log("Login button clicked");
+
+    // 1. Get the mobile number input
+    const numberInput = document.getElementById("input-number");
+    const mobileNumber = numberInput.value;
+    console.log(mobileNumber);
+
+    // 2. Get the pin input
+    const pinInput = document.getElementById("input-pin");
+    const pin = pinInput.value;
+    console.log(pin);
+
+    // 3. Match mobile number and pin
+    if(mobileNumber == "01410155520" && pin == "1234"){
+        // true > alert > homepage
+        alert("Login successfully");
+
+        window.location.assign("/home.html");
+    }
+    else{
+        // false > alert > return
+        alert("Login failed");
+        return;
+    }
+})
